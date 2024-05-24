64a393e42d25817dcaa75fb6ece17a72b44606f7
helissonpontes69@gmail.com <helissonpontes69@gmail.com>
2024-05-24 12:24:35 -0300

    aaaab

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -17,11 +17,64 @@ class CreateOrderService {
 
     const customerExists = await customersRepository.findById(customer_id);
 
-    if (customerExists) {
+    if (!customerExists) {
       throw new AppError('Could not find any customer with the given id');
     }
 
     const existsProducts = await productsRepository.findAllByIds(products);
+
+    if (existsProducts.length) {
+      throw new AppError('Could not find any products with the given ids');
+    }
+
+    const existsProductsIds = existsProducts.map(product => product.id);
+
+    const checkInexistentProducts = products.filter(
+      product => !existsProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id}.`,
+      );
+    }
+
+    const quantityAvailable = products.filter(
+      product =>
+        existsProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (quantityAvailable.length) {
+      throw new AppError(
+        `The quantity ${quantityAvailable[0].quantity}
+          is not available for ${quantityAvailable[0].id}.`,
+      );
+    }
+
+    const serializeProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existsProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await ordersRepository.createOrder({
+      customer: customerExists,
+      products: serializeProducts,
+    });
+
+    const { order_products } = order;
+
+    const updatedProductQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existsProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    await productsRepository.save(updatedProductQuantity);
+
+    return order;
   }
 }
 
